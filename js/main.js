b664353a70cf78f10d8edee55f2c5f138eb26011
Gabriel Austin <gabriel.austin2019@gmail.com>
2026-04-10 23:35:47 -0400

    mass updating HTML with append

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-let divElements = document.getElementsByClassName("cool-message");
+function createDivWithText(text) {
+  let div = document.createElement("div");
+  div.innerHTML = text;
+  return div;
+}
 
-console.log(divElements);
+let htmlText = ["Hello!", "I am EXTREMELY cool.", "That is because I am a programmer."];
+let container = document.getElementById("container");
+
+for (let i = 0; i < htmlText.length; i++) {
+  let newDiv = createDivWithText(htmlText[i]);
+  container.append(newDiv);
+}
