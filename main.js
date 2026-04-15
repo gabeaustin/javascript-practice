3ceebe03aa3531972f00c8e9b9d59a0b35c1473a
Gabriel Austin <gabriel.austin2019@gmail.com>
2026-04-14 21:44:49 -0400

    added button and event listener

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-function createDivWithText(text) {
-  let div = document.createElement("div");
-  div.innerHTML = text;
-  return div;
-}
+// function createDivWithText(text) {
+//   let div = document.createElement("div");
+//   div.innerHTML = text;
+//   return div;
+// }
 
-let htmlText = ["Hello!", "I am EXTREMELY cool.", "That is because I am a programmer."];
-let container = document.getElementById("container");
+// let htmlText = ["Hello!", "I am EXTREMELY cool.", "That is because I am a programmer."];
+// let container = document.getElementById("container");
 
-for (let i = 0; i < htmlText.length; i++) {
-  let newDiv = createDivWithText(htmlText[i]);
-  container.append(newDiv);
-}
+// for (let i = 0; i < htmlText.length; i++) {
+//   let newDiv = createDivWithText(htmlText[i]);
+//   container.append(newDiv);
+// }
+
+let button = document.getElementById("clickable-button");
+button.addEventListener("click", function () {
+  alert("I have been clicked");
+});
